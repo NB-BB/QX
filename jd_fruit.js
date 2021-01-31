adf2909477c276586e9ddc25b4c77ae8218fcb85
NB-BB <78240293+NB-BB@users.noreply.github.com>
2021-01-31 08:16:33 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -1281,12 +1281,11 @@ function shareCodesFormat() {
     }
     const readShareCodeRes = await readShareCode();
      
-if (readShareCodeRes && readShareCodeRes.code === 200) {
-
-$.newShareCodes =getRandomArr(getArrRandomly(readShareCodeRes.data),randomCount)
-
+    if (readShareCodeRes && readShareCodeRes.code === 200) {
+      // newShareCodes = newShareCodes.concat(readShareCodeRes.data || []);
+      newShareCodes = [...new Set([...newShareCodes, ...(readShareCodeRes.data || [])])];
     }
-    console.log(`第${$.index}个京东账号将要助力的好友${$.newShareCodes}`)
+    console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify(newShareCodes)}`)
     resolve();
   })
 }
